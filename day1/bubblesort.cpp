6bfd70b85883105c7f064cba1b0e2b4a1d54735c
July-jsy <930728875@qq.com>
2025-03-09 18:14:53 +0800

    July

--- day1/bubblesort.cpp
+++ day1/bubblesort.cpp
@@ -3,18 +3,20 @@ using namespace std;
 
 void bubbleSort(int arr[], int n)
 {
+    int temp;
     for (int i = 0; i < n - 1; i++)
     {
         for (int j = 0; j < n - i - 1; j++)
         {
             if (arr[j] < arr[j + 1])
             {
-                swap(arr[j], arr[j + 1]);
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
 }
-
 int main()
 {
     int arr[10] = {5, 3, 8, 1, 9, 2, 7, 4, 6, 0};
